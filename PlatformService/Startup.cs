bd3a8eabb69044ff2ba36a104eb0f4141522b1c8
Rodrigo Lins <rlinsdev@gmail.com>
2022-12-29 08:42:01 -0600

    Register Interface to concrete class in conf services

--- PlatformService/Startup.cs
+++ PlatformService/Startup.cs
@@ -30,6 +30,9 @@ namespace PlatformService
         {
             services.AddDbContext<AppDbContext>(opt => 
                 opt.UseInMemoryDatabase("InMem"));
+
+            services.AddScoped<IPlataformRepo, PlatformRepo>();
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
