e381ed69568e52ed530df0ac123f4d247f9384a1
Rodrigo Lins <rlinsdev@gmail.com>
2022-12-29 08:23:45 -0600

    Initialize - PlatformRepo.cs

---
+++ PlatformService/Data/PlatformRepo.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+  public class PlatformRepo : IPlataformRepo
+  {
+    public void CreatePlatform(Platform plat)
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public IEnumerable<Platform> GetAllPlatforms()
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public Platform GetPlatformById(int id)
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public bool SaveChanges()
+    {
+      throw new System.NotImplementedException();
+    }
+  }
+}
