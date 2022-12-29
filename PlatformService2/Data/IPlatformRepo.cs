7793cd2e04e5e8fc07acdb201f8868668057704c
Rodrigo Lins <rlinsdev@gmail.com>
2022-12-29 11:36:37 -0600

    Interface

---
+++ PlatformService2/Data/IPlatformRepo.cs
@@ -0,0 +1,13 @@
+using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+  public interface IPlataformRepo
+  {
+    bool SaveChanges();
+
+    IEnumerable<Platform> GetAllPlatforms();
+    Platform GetPlatformById(int id);
+    void CreatePlatform(Platform plat);
+  }
+}
