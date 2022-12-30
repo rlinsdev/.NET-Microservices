d9f95bd8f3d2f188d2ca6b8831a07c8074b5e2a8
Rodrigo Lins <rlinsdev@gmail.com>
2022-12-30 09:30:30 -0600

    Platforms Controller - CommandsService

---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/c/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        public PlatformsController()
+        {
+            
+        }
+
+        [HttpPost]
+        public ActionResult TestInBoundConnection()
+        {
+            Console.WriteLine("--> Inbound Post # Command Service");
+            return Ok("Inbound test of From Plataforms Controller (From CommandsService)");
+        }
+    }
+}
